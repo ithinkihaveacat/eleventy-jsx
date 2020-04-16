0f2b8f358887581c0a97682ab859672cadbbecef
Michael Stillwell <mjs@beebo.org>
2020-04-16 18:19:25 +0100

    Delete jsx.js

--- jsx.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const h = require("vhtml");
-exports.React = {
-    createElement: h,
-    Fragment: ({ children }) => h(null, null, ...children),
-};
